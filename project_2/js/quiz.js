81e2b95d5a65aebea265c4013bffab90816e45d1
Viktoryia <manuelka.wikta@gmail.com>
2023-10-17 20:17:14 +0200

    add id and startQuiz function

--- project_2/js/quiz.js
+++ project_2/js/quiz.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const startForm = document.getElementById("entry_form_container");
+const entryButton = document.getElementById("enter-btn");
+const questionContainerElement = document.getElementById("question_container")
+
+
+const startQuiz = () => {
+  startForm.classList.add('hidden')
+  questionContainerElement.classList.remove('hidden')
+}
+
+entryButton.addEventListener('click', startQuiz)
