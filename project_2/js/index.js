1b268b10dd187b750265772c735d39130c7e6d31
Kamil Kuziora <kam.kuziora@gmail.com>
2023-11-02 22:43:10 +0100

    added recent results

--- project_2/js/index.js
+++ project_2/js/index.js
@@ -1,52 +1,52 @@
 const backendURL = "http://localhost:8080";
 
-
 const ifLoggedIn = () => {
     const username = localStorage.getItem("username");
-    const userID = localStorage.getItem("id")
+    const userID = localStorage.getItem("id");
     const authorizationReq = document.querySelector(".auth_req");
     const menu = document.querySelector(".menu");
     const cookies = document.cookie.split("; ");
     const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
 
     const token = tokenCookie ? tokenCookie.split("=")[1] : null;
-    
 
-    if(token && username && userID){
-        authorizationReq.classList.add("hidden")
-        menu.classList.remove("hidden")
-        const usernameGreeting = document.querySelector(".menu_greeting")
-        usernameGreeting.innerText = `Witaj ${username}`
+    if (token && username && userID) {
+        authorizationReq.classList.add("hidden");
+        menu.classList.remove("hidden");
+        const usernameGreeting = document.querySelector(".menu_greeting");
+        usernameGreeting.innerText = `Witaj ${username}`;
     }
-}
+};
 
+window.onload = ifLoggedIn;
 
-    window.onload = ifLoggedIn
+const startQuizButton = document.querySelector(".start_quiz_btn");
 
-    const startQuizButton = document.querySelector(".start_quiz_btn")
-    
-    startQuizButton.addEventListener("click", async () => {
-        const token = getTokenFromCookie();
-    
-        const response = await fetch(`${backendURL}/quiz`, {
+startQuizButton.addEventListener("click", async () => {
+    const token = getTokenFromCookie();
+
+    const response = await fetch(`${backendURL}/quiz`, {
         method: "GET",
         headers: {
             Authorization: `Bearer ${token}`,
         },
-        });
-        const responseJson = await response.json();
-        localStorage.setItem("questions", JSON.stringify(responseJson)) 
-        window.location.pathname = "/pages/quiz.html"
-    })
+    });
+    const responseJson = await response.json();
+    localStorage.setItem("questions", JSON.stringify(responseJson));
+    window.location.pathname = "/pages/quiz.html";
+});
 
+const viewHistoryButton = document.querySelector('.history_quiz_btn');
 
-
+viewHistoryButton.addEventListener("click", ()=>{
+    window.location.pathname = '/pages/latest_results.html'
+})
 
 const getTokenFromCookie = () => {
     const cookies = document.cookie.split("; ");
     const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
-  
+
     const token = tokenCookie ? tokenCookie.split("=")[1] : null;
     console.log(token);
     return token;
-  };
+};
---
+++ project_2/js/latest_results.js
@@ -0,0 +1,61 @@
+window.onload = async () => {
+  const backendURL = "http://localhost:8080";
+  const cookies = document.cookie.split("; ");
+  const tokenCookie = cookies.find((cookie) => cookie.startsWith("token="));
+
+  const token = tokenCookie ? tokenCookie.split("=")[1] : null;
+
+  const userID = localStorage.getItem("id");
+
+  if (!token || !userID) {
+    window.location.pathname = "/";
+    return;
+  }
+
+  const response = await fetch(
+    `${backendURL}/quiz/get-user-results?id=${userID}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+
+  const responseJson = await response.json();
+
+  const containerElem = document.querySelector(".container");
+
+  responseJson.forEach((result) => {
+    containerElem.appendChild(
+      createResultContainer(result.timestamp, result.score),
+    );
+  });
+};
+
+const createResultContainer = (date, score) => {
+  const dateObj = new Date(date);
+
+  const formatOptions = {
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const formatter = new Intl.DateTimeFormat('pl-PL', formatOptions);
+  const formattedDate = formatter.format(dateObj);
+
+  const resContainer = document.createElement("div");
+  resContainer.classList.add("result_box");
+  const dateElem = document.createElement("p");
+  const scoreElem = document.createElement("p");
+  dateElem.innerText = `Data: ${formattedDate}`;
+  scoreElem.innerText = `Punkty: ${score}`;
+  resContainer.appendChild(dateElem);
+  resContainer.appendChild(scoreElem);
+
+  return resContainer;
+};
